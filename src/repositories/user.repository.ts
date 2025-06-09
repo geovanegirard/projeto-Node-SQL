8ddc5babfd8f6816e2538e4fef5e9d712659d8a3
Geovane Girard <g.ng2009@hotmail.com>
2025-06-09 11:39:28 -0300

    Developing user insertion.

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -10,10 +10,41 @@ class UserRepository {
         FROM application_User
         `;
 
-        const result = await db.query<User>(query);
-        const rows = result.rows;
+        const { rows } = await db.query<User>(query);
         return rows || [];
     }
+
+    async findById(uuid: string): Promise<User> {
+        const query = `
+        SELECT uuid, username 
+        FROM application_User
+        WHERE uuid = $1
+        `;
+
+        const values = [uuid];
+
+        const { rows } = await db.query<User>(query, values);
+        const [user] = rows;
+
+        return user;
+    }
+
+    async creat(user: User): Promise<string> {
+        const script = `
+        INSERT INTO application_user (
+            username,
+            password
+        )
+        VALUES ($1, crypt($2, 'my_salt'))
+        RETURNING uuid
+        `;
+
+        const values = [user,username, user.password];
+
+        const { rows } = await db.query<{ uuid: string }>(script, values);
+        const [newUser] = rows;
+        return newUser.uuid;
+    };
 
 }
 
